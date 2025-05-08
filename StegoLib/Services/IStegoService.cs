640f8c3988a4db299d270a88519c471d45a7e143
Bernie <ptyc4076@gmail.com>
2025-05-09 02:12:32 +0800

    加入專案檔案。

---
+++ StegoLib/Services/IStegoService.cs
@@ -0,0 +1,16 @@
+﻿using StegoLib.Models;
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StegoLib.Services
+{
+    internal interface IStegoService
+    {
+        StegoResult Embed(Bitmap coverImage, string message);
+        StegoResult Extract(Bitmap stegoImage);
+    }
+}
